fd44702f0ccc77c9c5d46f91ffb1ecf9c11aa888
Lucas <silvalucs212@gmail.com>
2021-09-22 08:35:00 -0300

    Primeiros passos com js

---
+++ Primeira pagina web/assets/js/script.js
@@ -0,0 +1,11 @@
+let nome = window.document.getElementById("nome");
+
+/*
+Case Sensitive = reconhece letras maiusculas e minusculas
+
+por Tag: getElementByTagName()
+por Id: getElementById()
+por Nome: getElementsByName()
+por CLasse: getElementsByClassName()
+por Seletor: querySelector()
+*/
